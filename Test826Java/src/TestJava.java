165bbbf1c7424af10ecae2b0fcf7091d10133b42
User <User@183.101.2.1>
2021-12-13 19:24:37 +0900

    커밋 5.0

--- Test826Java/src/TestJava.java
+++ Test826Java/src/TestJava.java
@@ -3,7 +3,7 @@ public class TestJava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GIT World!");
-		System.out.println("Version : 4.0");
+		System.out.println("Version : 5.0");
 		System.out.println("로그인 됨!!!");
 
 	}
